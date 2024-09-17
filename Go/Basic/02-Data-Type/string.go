16b75f019c0051ba685aa9fbc56c2065fc727dd9
petrushandika <petrushandikasinaga@gmail.com>
2024-09-17 10:45:59 +0700

    Data Type Go

---
+++ Go/Basic/02-Data-Type/string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Petrus")
+	fmt.Println("Petrus Handika")
+	fmt.Println("Petrus Handika Sinaga")
+
+	fmt.Println(len("Petrus"))
+	fmt.Println("Petrus Handika"[0])
+	fmt.Println("Petrus Handika Sinaga"[1])
+}
