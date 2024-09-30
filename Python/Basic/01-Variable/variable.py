cbe0cb37c7ede9f0ac94c476be531a86dfe2aa5a
petrushandika <petrushandikasinaga@gmail.com>
2024-09-30 22:21:27 +0700

    Variable Python

---
+++ Python/Basic/01-Variable/variable.py
@@ -0,0 +1,25 @@
+# variable are places to store data
+
+# assignment value
+x = 10
+y = 5
+z = 1000
+
+# calling first value
+print("Value x = ", x)
+print("Value x = ", y)
+print("Value x = ", z)
+
+# naming:
+# using underscore
+# pascal case
+# do not use number first for variable
+
+# calling second value
+x = 7
+print("Value x = ", x)
+
+# assignment indirect
+o = x
+o = 9
+print("value o = ", o)
