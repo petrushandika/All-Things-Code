022dfe9f73657a4a87d38651988f8f5c8d1161df
petrushandika <petrushandikasinaga@gmail.com>
2024-10-02 11:53:05 +0700

    Operation Logical Python

---
+++ Python/Basic/02-Data-Type/boolean.py
@@ -0,0 +1,77 @@
+# logical operation or boolean
+# not, or, and xor
+
+x = False
+y = not x
+print("data x = ", x)
+
+print("---------------")
+
+print("data y = ", y)
+
+# OR (if one value true, so the result is true)
+x = False
+y = False
+z = x or y
+print(x, "OR", y, "=", z)
+
+x = False
+y = True
+z = x or y
+print(x, "OR", y, "=", z)
+
+x = True
+y = False
+z = x or y
+print(x, "OR", y, "=", z)
+
+x = False
+y = False
+z = x or y
+print(x, "OR", y, "=", z)
+
+print("---------------")
+
+# AND (if one value false, so the result is false)
+x = False
+y = False
+z = x and y
+print(x, "AND", y, "=", z)
+
+x = False
+y = True
+z = x and y
+print(x, "AND", y, "=", z)
+
+x = True
+y = False
+z = x and y
+print(x, "AND", y, "=", z)
+
+x = False
+y = False
+z = x and y
+print(x, "AND", y, "=", z)
+
+print("---------------")
+
+# XOR (will be true if one value true, the rest is false)
+x = False
+y = False
+z = x ^ y
+print(x, "XOR", y, "=", z)
+
+x = False
+y = True
+z = x ^ y
+print(x, "XOR", y, "=", z)
+
+x = True
+y = False
+z = x ^ y
+print(x, "XOR", y, "=", z)
+
+x = False
+y = False
+z = x ^ y
+print(x, "XOR", y, "=", z)
---
+++ Python/Basic/02-Data-Type/comparison.py
@@ -0,0 +1,81 @@
+# comparison operations
+x = 4
+y = 2
+
+# greater then (>)
+result = x > 3
+print(x, ">", 3, "=", result)
+
+result = y > 3
+print(x, ">", 3, "=", result)
+
+result = y > 2
+print(x, ">", 2, "=", result)
+
+print("---------------")
+
+# less then (<)
+result = x < 3
+print(x, "<", 3, "=", result)
+
+result = y < 3
+print(y, "<", 3, "=", result)
+
+result = y < 2
+print(y, "<", 2, "=", result)
+
+print("---------------")
+
+# greater equals then (>=)
+result = x >= 3
+print(x, ">=", 3, "=", result)
+
+result = y >= 3
+print(y, ">=", 3, "=", result)
+
+result = y >= 2
+print(y, ">=", 2, "=", result)
+
+print("---------------")
+
+# less equals then (<=)
+result = x <= 3
+print(x, "<=", 3, "=", result)
+
+result = y <= 3
+print(y, "<=", 3, "=", result)
+
+result = y <= 2
+print(y, "<=", 2, "=", result)
+
+print("---------------")
+
+# equals then
+result = x == 4
+print(x, "==", 4, "=", result)
+
+result = y == 4
+print(y, "==", 4, "=", result)
+
+print("---------------")
+
+# not equals then
+result = x != 4
+print(x, "!=", 4, "=", result)
+
+result = y != 4
+print(y, "!=", 4, "=", result)
+
+print("---------------")
+
+# "is" to be comparison identity object
+x = 5
+y = 5
+print ("value x = ,", x, "id = ", hex(id(x)))
+print ("value y = ,", y, "id = ", hex(id(y)))
+
+result = x is y
+print("x is y = ", result)
+
+result = x is not y
+print("x is not y", result)
