91780da9a2c5b266c6ed51be94753ef158cd2beb
petrushandika <petrushandikasinaga@gmail.com>
2024-11-26 09:57:42 +0700

    learn goto Operator PHP

--- PHP/Basic/02-Data-Type/theory.php
+++ PHP/Basic/02-Data-Type/theory.php
@@ -44,3 +44,30 @@ function runApp($name) {
 }
 runApp("Petrus");
 ?>
+
+<!-- goto Operator -->
+<!-- 
+    - Salah satu fitur yang sebenarnya jarang sekali digunakan di PHP adalah goto operator
+    - Kenapa jarang digunakan karena jika terlalu banyak menggunakan goto operator, kode program aplikasinya akan mudah membingungkan yang membaca kode nya
+    - goto adalah fitur dimana kita bisa loncat ke kode program sesuai dengan keinginan kita
+    - Agar goto bisa loncat ke kode program, kita harus membuat label di php dengan menggunakan nama label lalu diakhiri : (colon)
+-->
+
+<?php
+// goto
+goto a;
+echo "Hello A" . PHP_EOL;
+
+a:
+echo "Hello A" . PHP_EOL;
+
+// goto Operator di Loop
+$counter = 1;
+while(true) {
+    echo "While Loop $counter" . PHP_EOL;
+    $counter++;
+    if ($counter > 10) {
+        break;
+    }
+}
+?>
