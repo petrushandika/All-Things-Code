cbc04b264fa79a7941e46cf70669067fb32f73da
petrushandika <petrushandikasinaga@gmail.com>
2024-12-15 10:29:17 +0700

    Perkenalan Algoritma

---
+++ Java/Basic/introduction.java
@@ -0,0 +1,13 @@
+package Basic; // directory folder
+
+public class introduction {
+    // Public static void main (spvm) -> metode utama yang dieksekusi saat program dijalankan dan tempat program Java memulai eksekusinya
+    public static void main(String[] args) {
+        // System out println (sout) -> mencetak teks dan mengganti baris baru setelahnya
+        System.out.println("Hello World");
+        // mencetak teks tanpa mengganti baris baru setelahnya
+        System.out.print("Hello World");
+        // print format -> mencetak teks dengan format tertentu
+        System.out.printf("%s%s", "My Names is", "Petrus Handika");
+    }
+}
